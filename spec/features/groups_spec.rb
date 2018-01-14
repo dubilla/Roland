ceccc9dcc6bccd94b81110c4f78ef2f0b7081837
dubilla <dan.ubilla@gmail.com>
2018-01-13 20:24:52 -0500

    Ensure only admins can create groups

--- spec/features/groups_spec.rb
+++ spec/features/groups_spec.rb
@@ -1,21 +1,18 @@
 require "rails_helper"
 
 RSpec.feature "Groups", type: :feature do
-  let(:user) { create :user, email: 'jason.day@golf.com' }
+  let(:user) { create :user, email: "jason.day@golf.com" }
   let(:tournament) { create :tournament }
 
-  context "i am logged out" do
+  context "i am a non-admin" do
+    let(:user) { create :user, email: 'jason.day@golf.com' }
     let!(:group) { create :group, name: "Open Group" }
 
     scenario "viewing groups" do
-      as_a_guest
+      as_a_user_i_login user
       and_i_visit_the_groups_page
       i_can_see_the_groups
       and_i_cannot_create_a_group
-    end
-
-    def as_a_guest
-      # no-op
     end
 
     def i_can_see_the_groups
@@ -27,9 +24,10 @@ RSpec.feature "Groups", type: :feature do
     end
   end
 
-  context "there are no groups" do
+  context "i am an admin and there are no groups" do
+    let(:admin) { create :admin, email: "admin@golf.com" }
     scenario "creating a group" do
-      as_a_user_i_login user
+      as_a_user_i_login admin
       and_i_visit_the_groups_page
       i_see_there_are_no_groups
       i_can_create_a_group
@@ -46,6 +44,11 @@ RSpec.feature "Groups", type: :feature do
       expect(page).to have_text 'Create Group'
       fill_in 'Name', with: 'Pool'
       click_button 'Create Group'
+    end
+
+    def and_see_the_group_page
+      expect(page).to have_text 'Pool'
+      expect(page).to have_text 'admin@golf.com'
     end
   end
 
@@ -79,10 +82,5 @@ RSpec.feature "Groups", type: :feature do
 
   def and_i_visit_the_groups_page
     visit groups_path
-  end
-
-  def and_see_the_group_page
-    expect(page).to have_text 'Pool'
-    expect(page).to have_text 'jason.day@golf.com'
   end
 end
