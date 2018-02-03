e31b7e105ac28ca22948c1497b691fc4435fddb2
dubilla <dan.ubilla@gmail.com>
2018-02-03 18:16:50 -0500

    Clean up the original matchup logic

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,6 +1,7 @@
 class Tournament < ActiveRecord::Base
   has_many :groups, through: :group_tournaments
   has_many :slots
+  has_many :matchups, through: :slots
 
   scope :unstarted, -> { where("start_date >= ? ", Date.today) }
 end
